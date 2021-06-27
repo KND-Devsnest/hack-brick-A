b0bbbf0ed5717c8d9e6f3f0bf5ee13cde2b783be
seebham <shubhamisdev@gmail.com>
2021-06-28 02:24:26 +0530

    Landing and Game Pages v1

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+var player = {
+  name: 'CoolDigger2608',
+  gender: '👷‍♂️'
+};
 function toggleVisibility(elem1, elem2) {
   elem1.classList.toggle('hidden');
   elem2.classList.toggle('hidden');
@@ -9,8 +13,23 @@ function toggleVol() {
   toggleVisibility(volOn, volOff);
 }
 
-function toggleGenderSelect() {
+function selectGender(gender) {
   let manSelect = document.getElementById('manSelect');
   let womanSelect = document.getElementById('womanSelect');
-  toggleVisibility(manSelect, womanSelect)
+  if(gender === 'female'){
+    player.gender = '👷‍♂️';
+    womanSelect.classList.add('selected');
+    manSelect.classList.remove('selected');
+  } else {
+    player.gender = '👷‍♀️';
+    manSelect.classList.add('selected');
+    womanSelect.classList.remove('selected');
+  }
+}
+
+function startGame() {
+  player.name = document.getElementById('playerNameInput').value;
+  document.getElementById('playerNameInput').value = "";
+  document.querySelector('.content').style.display = "none";
+  document.querySelector('#canvas-container').style.display = "block";
 }
