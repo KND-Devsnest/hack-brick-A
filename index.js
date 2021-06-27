54e0e188b3c76bc7a1b1f284c326e7618ee821eb
aguyran <59661860+aguyran@users.noreply.github.com>
2021-06-27 12:39:06 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 let x = 100;
 let y = 100;
 let xspeed = 1;
-let yspeed = 3.5;
+let yspeed = 2.8;
 let pos = 20;
 const canvas = document.getElementById("main");
-
+const level1 = ["100", "110", "120", "130", "140", "150", "160", "170"];
 ctx = canvas.getContext("2d");
 function draw(evt) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBoard();
 
   x += xspeed;
   y += yspeed;
@@ -24,7 +25,7 @@ function draw(evt) {
   if (y < 0) {
     yspeed = yspeed * -1;
   }
-  if (x >= pos && x <= pos + 45 && y >= 450 && y <= 460) {
+  if (x >= pos && x <= pos + 50 && y >= 450 && y <= 460) {
     yspeed = yspeed * -1;
   }
 }
@@ -35,3 +36,12 @@ setInterval(() => {
 canvas.addEventListener("mousemove", (e) => {
   pos = e.clientX;
 });
+function drawBoard() {
+  for (let i of level1) {
+    ctx.beginPath();
+    ctx.rect(i, 30, 50, 10);
+    ctx.stroke();
+  }
+}
+
+function checkCollision() {}
