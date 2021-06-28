362de7abba7391d1014859d1e4ff098b15461fdf
mrudula umalkar <mrudulaumalkar2001@gmail.com>
2021-06-28 17:14:42 +0530

    name and gender done

--- main.js
+++ main.js
@@ -14,23 +14,32 @@ function toggleVol() {
 }
 
 function selectGender(gender) {
+
   let manSelect = document.getElementById("manSelect");
   let womanSelect = document.getElementById("womanSelect");
   if (gender === "female") {
-    player.gender = "👷‍♂️";
+    player.gender = "👷‍♀️";
     womanSelect.classList.add("selected");
     manSelect.classList.remove("selected");
   } else {
-    player.gender = "👷‍♀️";
+    player.gender = "👷‍♂️";
     manSelect.classList.add("selected");
     womanSelect.classList.remove("selected");
   }
+  
 }
-
+const name1= document.querySelector('.title');
+console.log(player);
 function startGame() {
+  
   player.name = document.getElementById("playerNameInput").value;
   document.getElementById("playerNameInput").value = "";
   document.querySelector(".content").style.display = "none";
   document.querySelector("#canvas-container").style.display = "block";
+  name1.innerHTML=player.name+" "+player.gender;
+ 
   start();
+ 
 }
+
+
